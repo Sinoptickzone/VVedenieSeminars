bad32ee4120e400cda687505480b1ed90c0bf40b
Dmitry Kuzmin <sinoptickzone@gmail.com>
2023-03-10 17:45:39 +0300

    Added Seminar1, Seminar1_Homework

---
+++ Seminar1/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 1
+
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine()!);
+
+// int A = -N;
+
+// while(A <= N)
+// {
+//     Console.Write($"{A} ");
+//     A++;
+// }
+
+// Задача 2
+
+// Console.Write("Введите трехзнаное число: ");
+
+// int number = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(number%10);
+
+
+// Задача 3
+
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(Convert.ToInt32(Math.Pow(N, 2)));
+
+// Console.Write($"Квадрат числа: {N*N}");
+
+// Задача 4
+
+// Console.Write("Введите номер дня недели: ");
+// int N = int.Parse(Console.ReadLine()!);
+
+// switch(N)
+// {
+//     case 1:
+//     Console.WriteLine("Понедельник");
+//     break;
+
+//     case 2:
+//     Console.WriteLine("Вторник");
+//     break;
+
+//     case 3:
+//     Console.WriteLine("Среда");
+//     break;
+
+//     case 4:
+//     Console.WriteLine("Четверг");
+//     break;
+
+//     case 5:
+//     Console.WriteLine("Пятница");
+//     break;
+
+//     case 6:
+//     Console.WriteLine("Суббота");
+//     break;
+
+//     case 7:
+//     Console.WriteLine("Воскресенье");
+//     break;
+
+//     default:
+//     Console.WriteLine("Такого дня недели не существует!");
+//     break;
+// }
